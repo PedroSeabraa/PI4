20da90693ad7913abe09065a5ae77152f06eed06
grupo AI3 <58667373+PedroSeabraa@users.noreply.github.com>
2021-02-16 18:20:51 +0000

    Update dbconfig.php

--- dbconfig.php
+++ dbconfig.php
@@ -1,8 +1,8 @@
 <?php
-$server_name = "localhost";
-$db_username = "root";
-$db_password = "";
-$db_name = "pi4";
+$server_name = "us-cdbr-east-03.cleardb.com";
+$db_username = "b8223fae969424";
+$db_password = "f012793b";
+$db_name = "heroku_0a48125453dc4de";
 
 $connection = mysqli_connect($server_name,$db_username,$db_password);
 $dbconfig = mysqli_select_db($connection,$db_name);
