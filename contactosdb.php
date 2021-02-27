78b836cbf3f7f9ed69a5ecefd29203a2c68ba372
grupo AI3 <58667373+PedroSeabraa@users.noreply.github.com>
2021-02-27 18:15:01 +0000

    Update contactosdb.php

--- contactosdb.php
+++ contactosdb.php
@@ -1,22 +1,10 @@
 <?php
 
-//host name
-$host = "us-cdbr-east-03.cleardb.com";
-//user name
-$username = "b8223fae969424";
-//database password
-$pwd = "f012793b";
-//database name.
-$db = "heroku_0a48125453dc4de";
+require 'dbconfig.php';
 
-$con=mysqli_connect($host,$username,$pwd,$db) or die("Unable to Connect");
+session_start();
 
-if(mysqli_connect_error($con))
-{
-	echo "Failed to connect";
-}
-
-$query=mysqli_query($con, "SELECT * FROM contacto");
+$query=mysqli_query($connection, "SELECT * FROM contacto");
 
 if($query)
 {
@@ -26,7 +14,7 @@ while($row=mysqli_fetch_array($query))
 	}
 print(json_encode($flag));
 }
-mysqli_close($con);
+mysqli_close($connection);
 
 
 ?>
