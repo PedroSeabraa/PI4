2ee66d549404b0a4313af242187a39c7d6a5c6e0
grupo AI3 <58667373+PedroSeabraa@users.noreply.github.com>
2021-02-27 19:18:41 +0000

    Update ListaInscritos.php

--- ListaInscritos.php
+++ ListaInscritos.php
@@ -1,22 +1,8 @@
 <?php
 
-//host name
-$host = "us-cdbr-east-03.cleardb.com";
-//user name
-$username = "b8223fae969424";
-//database password
-$pwd = "f012793b";
-//database name.
-$db = "heroku_0a48125453dc4de";
+require 'dbconfig.php';
 
 session_start();
-
-$con=mysqli_connect($host,$username,$pwd,$db) or die("Unable to Connect");
-
-if(mysqli_connect_error($con))
-{
-	echo "Failed to connect";
-}
 
 if (isset($_GET['Holderid'])) {
     $id = $_GET['Holderid'];
@@ -26,7 +12,7 @@ else
     $id = "0";
 }
 
-$query=mysqli_query($con, "SELECT * FROM utilizadores WHERE evento_id_evento ='$id'");
+$query=mysqli_query($connection, "SELECT * FROM utilizadores WHERE evento_id_evento ='$id'");
 
 if($query)
 {
@@ -36,7 +22,7 @@ while($row=mysqli_fetch_array($query))
 	}
 print(json_encode($flag));
 }
-mysqli_close($con);
+mysqli_close($connection);
 
 ?>
 
