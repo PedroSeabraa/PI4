41445194814068b3837a0c01a44ebda6b3969576
grupo AI3 <58667373+PedroSeabraa@users.noreply.github.com>
2021-02-27 19:01:08 +0000

    Update basedados.php

--- basedados.php
+++ basedados.php
@@ -1,6 +1,24 @@
 <?php
 
-require 'dbconfig.php';
+$server_name = "us-cdbr-east-03.cleardb.com";
+$db_username = "b8223fae969424";
+$db_password = "f012793b";
+$db_name = "heroku_0a48125453dc4de";
+
+$connection = mysqli_connect($server_name,$db_username,$db_password);
+$dbconfig = mysqli_select_db($connection,$db_name);
+
+
+if($dbconfig)
+{
+    //echo "database connected";
+    
+}
+else{
+    echo '
+   
+    ';
+}
 
 session_start();
 
