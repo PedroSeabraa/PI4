3770b078e87ecd83e42ea5d3c2728923bfc13169
grupo AI3 <58667373+PedroSeabraa@users.noreply.github.com>
2021-03-01 22:45:51 +0000

    Update inseriradmin.php

--- backoffice/inseriradmin.php
+++ backoffice/inseriradmin.php
@@ -69,7 +69,25 @@ if(empty($_SESSION))
   echo "<script> location.href='login.php'; </script>";
 }
 
-require 'dbconfig.php';
+$server_name = "us-cdbr-east-03.cleardb.com";
+$db_username = "b8223fae969424";
+$db_password = "f012793b";
+$db_name = "heroku_0a48125453dc4de";
+
+$connection = mysqli_connect($server_name,$db_username,$db_password);
+$dbconfig = mysqli_select_db($connection,$db_name);
+
+
+if($dbconfig)
+{
+    //echo "database connected";
+    
+}
+else{
+    echo '
+   
+    ';
+}
 
 $sql="";
 
