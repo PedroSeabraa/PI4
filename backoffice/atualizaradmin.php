83ebd4bd9e868e388f7a1a9dad43f9db1a1ed713
grupo AI3 <58667373+PedroSeabraa@users.noreply.github.com>
2021-03-01 21:47:08 +0000

    Update atualizaradmin.php

--- backoffice/atualizaradmin.php
+++ backoffice/atualizaradmin.php
@@ -15,13 +15,13 @@
     </li>
     <li>
       <a href="#">
-        <i class="fa fa-tachometer" aria-hidden="true" ></i> <a href="Admin.php">Admin</a>
+        <i class="fa fa-tachometer" aria-hidden="true" ></i> <a href="admin.php">Admin</a>
 
       </a>
     </li>
 			<li>
       <a href="#">
-        <i class="fa fa-tachometer" aria-hidden="true" ></i> <a href="Eventos.php">Eventos</a>
+        <i class="fa fa-tachometer" aria-hidden="true" ></i> <a href="eventos.php">Eventos</a>
 
       </a>
     </li>
