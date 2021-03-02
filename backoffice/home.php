8bab7068c7e63d35962981fd43cbca5f3ff0e5e5
grupo AI3 <58667373+PedroSeabraa@users.noreply.github.com>
2021-03-02 17:25:31 +0000

    Update home.php

--- backoffice/home.php
+++ backoffice/home.php
@@ -60,7 +60,7 @@
     <!-- Main component for a primary marketing message or call to action -->
     <div class="jumbotron">
       <h1>Eventos Desportivos Penacova</h1>
-      <p>Alguma coisa!</p>
+      
       
     </div>
 
