079cb922ea24036c6b4dc263a10f5ffd26e61d00
grupo AI3 <58667373+PedroSeabraa@users.noreply.github.com>
2021-03-02 16:05:04 +0000

    Update authentication.php

--- backoffice/authentication.php
+++ backoffice/authentication.php
@@ -7,6 +7,13 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+	
+	<style>  
+	  
+	  #h1{ 
+		 margin-top:25%;
+	  }
+		</script>
 </head>
 <body>
 
